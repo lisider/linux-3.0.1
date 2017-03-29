277ad485380f82a5cde8ade297260a58587d9a30
ZhaoYang <zhaoyang-sky@outlook.com>
2017-03-29 09:16:28 +0800

    build the init project for ok6410. This is the example code which provided by the official website

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 671 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#671 Sat Sep 13 17:25:01 CST 2014"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.3.2 (Sourcery G++ Lite 2008q3-72) "
